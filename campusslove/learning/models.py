6ee701e4a033c5dbe8e254b530880a7b8aa35489
Ok-Ranjan <ranjanmp802114@gmail.com>
2026-03-14 23:43:47 +0530

    learning/models: add class LearningMaterial and do migrate

--- campusslove/learning/models.py
+++ campusslove/learning/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class LearningMaterial(models.Model):
+    user_id = models.IntegerField()
+    
+    title = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200)
+    topic = models.CharField(max_length=200)
+
+    notes = models.TextField(blank=True) # for writting notes
+
+    link = models.URLField(blank=True)
+
+    file = models.FileField(upload_to='learning_files/', blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
