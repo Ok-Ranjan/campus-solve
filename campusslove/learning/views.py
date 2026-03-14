0258bf57c6ad31af553d3e00e6962f8ef14210cc
Ok-Ranjan <ranjanmp802114@gmail.com>
2026-03-14 23:54:40 +0530

    learning/views: define add_learning function

--- campusslove/learning/views.py
+++ campusslove/learning/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import LearningMaterial
 
-# Create your views here.
+def add_learning(request):
+
+    if request.method == "POST":
+
+        title = request.POST['title']
+        subject = request.POST['subject']
+        topic = request.POST['topic']
+        notes = request.POST['notes']
+        link = request.POST['link']
+
+        material = LearningMaterial.objects.create(
+            user_id=1,
+            title=title,
+            subject=subject,
+            topic=topic,
+            notes=notes,
+            link=link
+        )
+
+    return render(request, "add_learning.html")
